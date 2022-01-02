36986afec1e44557fafc8f164a5a95ba3198a67b
dy-dx-1 <nic19sarbe@gmail.com>
2022-01-02 12:49:18 -0500

    starting creation of base

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def main():
-    pass 
+import tkinter as tk
+from typing import Collection 
+root = tk.Tk()
 
+def create_base():
+    # First let's create the base 
+    root.title("Calculator") 
+    root.geometry("400x600") 
+    # Placing the output label 
+    root.grid_columnconfigure(2, weight=1)
+    output_label = tk.Label(root, text = "THIS IS OUTPUT")
+    output_label.grid(row=0, column=0, columnspan=4)
+
+def main():
+    create_base() 
+    root.mainloop()
 if __name__ == "__main__": 
     main() 
